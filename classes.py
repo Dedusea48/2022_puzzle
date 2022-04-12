ac3e99a223d73cea9dafda91654492fc285c4da5
Дмитрий <vasilenko.dmitrii@phystech.edu>
2022-04-12 18:04:16 +0300

    made box pushing

--- classes.py
+++ classes.py
@@ -9,14 +9,26 @@ class Player(Block):
         super().__init__(x0, y0)
         self.step = 1
 
-    def move_up(self):
-        self.y -= self.step
+    def move_up(self, obj):
+        if obj.y == self.y + self.step:
+            self.obj += self.step
+        else:
+            self.y -= self.step
 
-    def move_down(self):
-        self.y += self.step
+    def move_down(self, obj):
+        if obj.y == self.y - self.step:
+            self.obj -= self.step
+        else:
+            self.y += self.step
 
-    def move_left(self):
-        self.x -= self.step
+    def move_left(self, obj):
+        if obj.x == self.x - self.step:
+            obj.x -= self.step
+        else:
+            self.x -= self.step
 
-    def move_right(self):
-        self.x += self.step
+    def move_right(self, obj):
+        if obj.x == self.x + self.step:
+            obj.x += self.step
+        else:
+            self.x += self.step
