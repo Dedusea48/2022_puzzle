d08eff6582bd6662152afbb9d21fc72fc4f5d4c8
Kutorkin Nikolay <kutorkin.ne@phystech.edu>
2022-04-15 16:29:44 +0300

    Сделал поле(платформу) и игрока, двигующегося по стрелкам, но НЕ сделал ограничения движения, он может выйти за массив. Всё рисуется, игрок двигается. Также добавил палитру. Пожалуйста, изучите поле(платформу)!

--- classes.py
+++ classes.py
@@ -1,3 +1,17 @@
+import pygame.draw as dr
+
+# Палитра
+RED = 0xFF0000
+BLUE = 0x0000FF
+YELLOW = 0xFFC91F
+GREEN = 0x00FF00
+MAGENTA = 0xFF03B8
+CYAN = 0x00FFCC
+BLACK = (0, 0, 0)
+WHITE = 0xFFFFFF
+GREY = 0x7D7D7D
+
+
 class Block:
     def __init__(self, x0, y0):
         self.x = x0
@@ -5,9 +19,16 @@ class Block:
 
 
 class Player(Block):
+    """
+    Создаёт игрока. При создании ему передаюся его координаты на платформе, а не на экране.
+    Знает, как нарисовать себя.
+    """
+
     def __init__(self, x0, y0):
         super().__init__(x0, y0)
         self.step = 1
+        self.side = 40
+        self.color = YELLOW
 
     def move_up(self, obj):
         if obj.y == self.y + self.step:
@@ -33,10 +54,113 @@ class Player(Block):
         else:
             self.x += self.step
 
-class Field:
-    def __init__(self, level_field):
-        self.level_field = None
-        self.level = level_field
+    def step_up(self, platform):
+        """
+        Делает шаг в верхнюю сторону.
+        :param platform: платформа, на которой находится игрок.
+        :return:
+        """
+        platform.squares[self.x][self.y][1] = None
+        self.y -= self.step
+        platform.squares[self.x][self.y][1] = self
+
+    def step_down(self, platform):
+        """
+        Делает шаг в нижнюю сторону.
+        :param platform: платформа, на которой находится игрок.
+        :return:
+        """
+        platform.squares[self.x][self.y][1] = None
+        self.y += self.step
+        platform.squares[self.x][self.y][1] = self
+
+    def step_left(self, platform):
+        """
+        Делает шаг в левую сторону.
+        :param platform: платформа, на которой находится игрок.
+        :return:
+        """
+        platform.squares[self.x][self.y][1] = None
+        self.x -= self.step
+        platform.squares[self.x][self.y][1] = self
+
+    def step_right(self, platform):
+        """
+        Делает шаг в правую сторону.
+        :param platform: платформа, на которой находится игрок.
+        :return:
+        """
+        platform.squares[self.x][self.y][1] = None
+        self.x += self.step
+        platform.squares[self.x][self.y][1] = self
+
+    def draw(self, screen, x, y):  # QUESTION Зачем? Можно же просто обратиться к полю.
+        dr.rect(screen, self.color, [x, y, self.side, self.side])
+
+
+class Platform:
+    """
+    Создаёт игровую клеточную платформу, на которой будут находится объекты.
+    Платформа состоит из клеток. Каждая клетка имеет два уровня: фундамент (0) и то, что стоит на фундаменте (1).
+
+    Чтобы что-то извлечь из платформы, нужно указать координаты клетки - x и y, потом указать уровень (0 или 1),
+    например Platform.squares[x][y][0] (извлечение объекта фундамента клетки с координатами x и y)
+
+    Платформа самостоятельно организовывает свой пол(фундамент) из плиток (Tile).
+    """
+
+    def __init__(self, horizontal_side, vertical_side, screen, x0, y0):
+        self.screen = screen
+        self.x0 = x0
+        self.y0 = y0
+
+        self.vertical_side = vertical_side
+        self.horizontal_side = horizontal_side
+        self.square_side = 40
+        self.colors = [GREEN, GREY]
+        self.squares = []
+
+        check = 0
+        for i in range(horizontal_side):
+            column = []
+            check = not check
+            for j in range(vertical_side):
+                tile = Tile(self.square_side, self.colors[check])
+                square = [tile, None]
+                column.append(square)
+                check = not check
+            self.squares.append(column)
 
     def size(self):
-        return len(self.level_field), len(self.level_field[0])
+        return self.vertical_side, self.horizontal_side
+
+    def draw(self):
+        """
+        Рисует все объекты, которые находятся в клетках (squares).
+        :return:
+        """
+        check = 0
+        for i in range(self.horizontal_side):
+            check = not check
+            for j in range(self.vertical_side):
+
+                for k in [0, 1]:
+                    if self.squares[i][j][k] is not None:
+                        self.squares[i][j][k].draw(self.screen, self.x0 + i * self.square_side,
+                                                   self.y0 + j * self.square_side)
+
+                check = not check
+
+
+class Tile:
+    """
+    Создаёт квадратную плитку данного цвета.
+    Знает, как себя нарисовать.
+    """
+
+    def __init__(self, side, color):
+        self.side = side
+        self.color = color
+
+    def draw(self, screen, x, y):
+        dr.rect(screen, self.color, [x, y, self.side, self.side])
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import classes
+import pygame
 
-level = classes.Field({{"0","_","0"},
-                       {"0","_","0"},
-                       {"0","_","0"}})
+WIDTH = 600
+HEIGHT = 600
+FPS = 30
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+finished = False
+level = classes.Platform(5, 10, screen, 100, 100)
+player = classes.Player(1, 2)
+level.squares[1][2][1] = player
+
+while not finished:
+    level.draw()
+    pygame.display.update()
+    clock.tick(FPS)
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.step_up(level)
+            elif event.key == pygame.K_DOWN:
+                player.step_down(level)
+            elif event.key == pygame.K_LEFT:
+                player.step_left(level)
+            elif event.key == pygame.K_RIGHT:
+                player.step_right(level)
