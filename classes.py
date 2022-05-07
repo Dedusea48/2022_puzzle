e8cd28145fc5a8a8618102ad5cd0e3860254f85b
Kutorkin Nikolay <kutorkin.ne@phystech.edu>
2022-05-07 21:55:12 +0300

    Первый этап сделан. Часть функционала работает

--- classes.py
+++ classes.py
@@ -3,6 +3,7 @@
 """
 
 import classes_back_obj as back
+import classes_front_obj as front
 import pygame
 
 # Палитра
@@ -27,13 +28,13 @@ class Level:
     Уровень самостоятельно организовывает свой пол из класса Floor.
     """
 
-    def __init__(self, horizontal_side, vertical_side, screen, x0, y0):
+    def __init__(self, screen, x0, y0, horizontal_side, vertical_side, player, finish):
         """
         Создает уровень, заполняя каждую клекту объектом Floor
         :param horizontal_side: кол-во клеток по горизонтали
         :param vertical_side: кол-во клеток по вертикали
         :param screen: экран
-        :param x0: x-координата верхнего левого угла уровня
+        :param x0: x-координата верхнего левого угла уровн
         :param y0: y-координата верхнего левого угла уровня
         """
         self.screen = screen
@@ -42,15 +43,17 @@ class Level:
 
         self.vertical_side = vertical_side
         self.horizontal_side = horizontal_side
-        self.colors = [GREEN, GREY]
+        self.size = 40
         self.tiles = []
+
+        self.player = player
+        self.finish = finish
 
         for i in range(horizontal_side):
             column = []
             for j in range(vertical_side):
-                tile = Tile(i, j, self.screen)
-                floor = back.Floor(tile, grass)
-                tile.back_obj = floor
+                floor = back.Floor(self.screen, grass)
+                tile = Tile(None, floor)
                 column.append(tile)
             self.tiles.append(column)
 
@@ -60,28 +63,80 @@ class Level:
         """
         for i in range(self.horizontal_side):
             for j in range(self.vertical_side):
-                self.tiles[i][j].draw(self.x0, self.y0)
+                if self.tiles[i][j].back_obj is not None:
+                    self.tiles[i][j].back_obj.draw(self.x0 + i * self.size, self.y0 + j * self.size)
 
-    def return_obj(self, obj):
-        """
-        Возвращает объект на уровне с названием obj
-        :param obj: название объекта
-        :return: объект
-        """
-        for column in self.tiles:
-            for tile in column:
-                if tile.return_back_obj() and tile.return_back_obj().name == obj:
-                    return tile.return_back_obj()
-                elif tile.return_front_obj() and tile.return_front_obj().name == obj:
-                    return tile.return_front_obj()
+        for i in range(self.horizontal_side):
+            for j in range(self.vertical_side):
+                if self.tiles[i][j].front_obj is not None:
+                    self.tiles[i][j].front_obj.draw(self.x0 + i * self.size, self.y0 + j * self.size)
 
-    def check_next_level(self):
-        """
-        Проверяет, находится ли игрок на клетке финиша
-        :return: True или False
-        """
-        return self.return_obj('next_level_tile').x == self.return_obj('player').x and \
-               self.return_obj('next_level_tile').y == self.return_obj('player').y
+    def player_move(self, direction):
+        y = self.player.y
+        x = self.player.x
+        if direction == 'up':
+            y -= 1
+        if direction == 'down':
+            y += 1
+        if direction == 'left':
+            x -= 1
+        if direction == 'right':
+            x += 1
+
+        if 0 <= x < self.horizontal_side and 0 <= y < self.vertical_side:
+
+            if self.tiles[x][y].front_obj is not None:
+                self.player_kick(x, y)
+            else:
+                self.player_step(x, y)
+
+    def player_kick(self, x, y):
+        if not isinstance(self.tiles[x][y].front_obj, front.Wall):
+            _x = x + (x - self.player.x)
+            _y = y + (y - self.player.y)
+
+            if (0 <= _x < self.horizontal_side and
+                    0 <= _y < self.vertical_side and
+                    self.tiles[_x][_y].front_obj is None):
+                self.tiles[_x][_y].front_obj = self.tiles[x][y].front_obj
+                self.tiles[x][y].front_obj = None
+                self.check_interaction()
+
+    def player_step(self, x, y):
+        if not isinstance(self.tiles[x][y].back_obj, back.Water):
+            self.tiles[self.player.x][self.player.y].front_obj = None
+            self.player.x = x
+            self.player.y = y
+            self.tiles[self.player.x][self.player.y].front_obj = self.player
+
+    def check_interaction(self):
+        for i in range(self.horizontal_side):
+            for j in range(self.vertical_side):
+                if (isinstance(self.tiles[i][j].back_obj, back.Water) and
+                        isinstance(self.tiles[i][j].front_obj, front.Box)):
+                    self.tiles[i][j].back_obj = self.tiles[i][j].front_obj
+                    self.tiles[i][j].front_obj = None
+
+    # def return_obj(self, obj):
+    #     """
+    #     Возвращает объект на уровне с названием obj
+    #     :param obj: название объекта
+    #     :return: объект
+    #     """
+    #     for column in self.tiles:
+    #         for tile in column:
+    #             if tile.return_back_obj() and tile.return_back_obj().name == obj:
+    #                 return tile.return_back_obj()
+    #             elif tile.return_front_obj() and tile.return_front_obj().name == obj:
+    #                 return tile.return_front_obj()
+    #
+    # def check_next_level(self):
+    #     """
+    #     Проверяет, находится ли игрок на клетке финиша
+    #     :return: True или False
+    #     """
+    #     return self.return_obj('next_level_tile').x == self.return_obj('player').x and \
+    #            self.return_obj('next_level_tile').y == self.return_obj('player').y
 
 
 class Tile:
@@ -89,37 +144,32 @@ class Tile:
     Игровая единица площади, которая содержит задние объекты (BackObj) и верхние (FrontObj).
     """
 
-    def __init__(self, x, y, screen):
+    def __init__(self, front_obj, back_obj):
         """
         Создает клетку
-        :param x: x-координата клетки
-        :param y: y-координата клетки
-        :param screen: экран
-        """
-        self.x = x
-        self.y = y
-        self.back_obj = None
-        self.front_obj = None
-        self.size = 40
-        self.screen = screen
 
-    def draw(self, x0, y0):
-        """Рисует клетку, то есть все объекты на клетке"""
-        if self.back_obj is not None:
-            self.back_obj.draw(x0, y0)
-        if self.front_obj is not None:
-            self.front_obj.draw(x0, y0)
+        """
+        self.back_obj = back_obj
+        self.front_obj = front_obj
+        # self.size = 40
 
-    def return_back_obj(self):
-        """
-        Возвращает задний объект на клетке
-        :return: задний объект
-        """
-        return self.back_obj
+    # def draw(self, x, y):
+    #     """Рисует клетку, то есть все объекты на клетке"""
+    #     if self.back_obj is not None:
+    #         self.back_obj.draw(x, y)
+    #     if self.front_obj is not None:
+    #         self.front_obj.draw(x, y)
 
-    def return_front_obj(self):
-        """
-        Возвращает передний объект на клетке
-        :return: передний объектч
-        """
-        return self.front_obj
+    # def return_back_obj(self):
+    #     """
+    #     Возвращает задний объект на клетке
+    #     :return: задний объект
+    #     """
+    #     return self.back_obj
+    #
+    # def return_front_obj(self):
+    #     """
+    #     Возвращает передний объект на клетке
+    #     :return: передний объектч
+    #     """
+    #     return self.front_obj
--- main.py
+++ main.py
@@ -14,51 +14,60 @@ clock = pygame.time.Clock()
 finished = [False]
 
 # LEVEL 1
-level1 = classes.Level(5, 5, screen, 100, 100)
-front.Player(level1.tiles[0][0], level1, images.upload_images_player())
-back.next_level_tile(level1.tiles[4][0], images.ladder())
 
-front.Box(level1.tiles[2][1], images.box_images())
-front.Box(level1.tiles[1][2], images.box_images())
+player = front.Player(screen, 0, 0, images.upload_images_player())
+# back.next_level_tile(level1.tiles[4][0], images.ladder())
+level1 = classes.Level(screen, 100, 100, 5, 5, player, None)
+level1.tiles[0][0].front_obj = player
+level1.tiles[2][1].front_obj = front.Box(screen, images.box_images())
+level1.tiles[1][2].front_obj = front.Box(screen, images.box_images())
 
-front.Wall(level1.tiles[0][1], images.wall())
-front.Wall(level1.tiles[1][1], images.wall())
-front.Wall(level1.tiles[3][0], images.wall())
-front.Wall(level1.tiles[3][1], images.wall())
-front.Wall(level1.tiles[1][3], images.wall())
 
-back.Water(level1.tiles[4][1], images.water_images())
-back.Water(level1.tiles[4][2], images.water_images())
+level1.tiles[0][1].front_obj = front.Wall(screen, images.wall())
+level1.tiles[1][1].front_obj = front.Wall(screen, images.wall())
+level1.tiles[3][0].front_obj = front.Wall(screen, images.wall())
+level1.tiles[3][1].front_obj = front.Wall(screen, images.wall())
+level1.tiles[1][3].front_obj = front.Wall(screen, images.wall())
+# front.Wall(level1.tiles[0][1], images.wall())
+# front.Wall(level1.tiles[1][1], images.wall())
+# front.Wall(level1.tiles[3][0], images.wall())
+# front.Wall(level1.tiles[3][1], images.wall())
+# front.Wall(level1.tiles[1][3], images.wall())
+
+level1.tiles[4][1].back_obj = back.Water(screen, images.water_images())
+level1.tiles[4][2].back_obj = back.Water(screen, images.water_images())
+# back.Water(level1.tiles[4][1], images.water_images())
+# back.Water(level1.tiles[4][2], images.water_images())
 
 # LEVEL 2
-level2 = classes.Level(6, 8, screen, 100, 100)
-front.Player(level2.tiles[0][7], level2, images.upload_images_player())
-back.next_level_tile(level2.tiles[5][7], images.ladder())
-
-front.Wall(level2.tiles[4][2], images.wall())
-front.Wall(level2.tiles[5][2], images.wall())
-front.Wall(level2.tiles[3][5], images.wall())
-front.Wall(level2.tiles[3][6], images.wall())
-front.Wall(level2.tiles[3][7], images.wall())
-
-back.Water(level2.tiles[0][3], images.water_images())
-back.Water(level2.tiles[1][3], images.water_images())
-back.Water(level2.tiles[2][3], images.water_images())
-back.Water(level2.tiles[3][3], images.water_images())
-back.Water(level2.tiles[4][3], images.water_images())
-back.Water(level2.tiles[5][3], images.water_images())
-back.Water(level2.tiles[0][4], images.water_images())
-back.Water(level2.tiles[1][4], images.water_images())
-back.Water(level2.tiles[2][4], images.water_images())
-back.Water(level2.tiles[3][4], images.water_images())
-back.Water(level2.tiles[4][4], images.water_images())
-back.Water(level2.tiles[5][4], images.water_images())
-
-front.Box(level2.tiles[1][2], images.box_images())
-front.Box(level2.tiles[4][1], images.box_images())
-front.Box(level2.tiles[1][5], images.box_images())
-front.Box(level2.tiles[1][6], images.box_images())
-front.Box(level2.tiles[2][5], images.box_images())
+level2 = classes.Level(6, 8, screen, 100, 100, player, None)
+# front.Player(level2.tiles[0][7], level2, images.upload_images_player())
+# back.next_level_tile(level2.tiles[5][7], images.ladder())
+#
+# front.Wall(level2.tiles[4][2], images.wall())
+# front.Wall(level2.tiles[5][2], images.wall())
+# front.Wall(level2.tiles[3][5], images.wall())
+# front.Wall(level2.tiles[3][6], images.wall())
+# front.Wall(level2.tiles[3][7], images.wall())
+#
+# back.Water(level2.tiles[0][3], images.water_images())
+# back.Water(level2.tiles[1][3], images.water_images())
+# back.Water(level2.tiles[2][3], images.water_images())
+# back.Water(level2.tiles[3][3], images.water_images())
+# back.Water(level2.tiles[4][3], images.water_images())
+# back.Water(level2.tiles[5][3], images.water_images())
+# back.Water(level2.tiles[0][4], images.water_images())
+# back.Water(level2.tiles[1][4], images.water_images())
+# back.Water(level2.tiles[2][4], images.water_images())
+# back.Water(level2.tiles[3][4], images.water_images())
+# back.Water(level2.tiles[4][4], images.water_images())
+# back.Water(level2.tiles[5][4], images.water_images())
+#
+# front.Box(level2.tiles[1][2], images.box_images())
+# front.Box(level2.tiles[4][1], images.box_images())
+# front.Box(level2.tiles[1][5], images.box_images())
+# front.Box(level2.tiles[1][6], images.box_images())
+# front.Box(level2.tiles[2][5], images.box_images())
 
 LEVELS = (level1, level2)
 cur_level = 0
@@ -76,24 +85,25 @@ def game_process(is_finished, cur_level):
             exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                LEVELS[cur_level].return_obj('player').move('up')
+                LEVELS[cur_level].player_move('up')
             elif event.key == pygame.K_DOWN:
-                LEVELS[cur_level].return_obj('player').move('down')
+                LEVELS[cur_level].player_move('down')
             elif event.key == pygame.K_LEFT:
-                LEVELS[cur_level].return_obj('player').change_sprites(images.upload_images_mirrored())
-                LEVELS[cur_level].return_obj('player').move('left')
-                LEVELS[cur_level].return_obj('player').to_left()
+                # LEVELS[cur_level].return_obj('player').change_sprites(images.upload_images_mirrored())
+                LEVELS[cur_level].player_move('left')
+                # LEVELS[cur_level].return_obj('player').to_left()
             elif event.key == pygame.K_RIGHT:
-                LEVELS[cur_level].return_obj('player').change_sprites(images.upload_images_player())
-                LEVELS[cur_level].return_obj('player').move('right')
-                LEVELS[cur_level].return_obj('player').to_right()
+                #LEVELS[cur_level].return_obj('player').change_sprites(images.upload_images_player())
+
+                LEVELS[cur_level].player_move('right')
+                #LEVELS[cur_level].return_obj('player').to_right()
             elif event.key == pygame.K_ESCAPE:
                 finished[0] = True
 
-    LEVELS[cur_level].return_obj('player').update(0.5)
+    LEVELS[cur_level].player.update(0.5)
     # FIXME должно работать для всех уровней
     # water_block1.update(0.2)
     # water_block2.update(0.2)
-    if LEVELS[cur_level].check_next_level():
-        cur_level += 1
+    # if LEVELS[cur_level].check_next_level():
+    #     cur_level += 1
     return cur_level
